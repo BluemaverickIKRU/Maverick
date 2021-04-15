3ec6ab1cc6f3d9ac29d742e3de48a668dbf85277
Rajasekar <73211169+BluemaverickIKRU@users.noreply.github.com>
2021-04-15 09:10:25 +0530

    Update TrendyorNot.py

--- TrendyorNot.py
+++ TrendyorNot.py
@@ -1,12 +1,8 @@
 times = int(input())
-p = 0
 num_list = []
-while True:
-    p += 1
-    num = input()
+for i in range(times):
+    num = int(input())
     num_list.append(num)
-    if p == times:
-        break
 for i in num_list:
     if int(i[1]) % 3 == 0:
         print("Trendy number")
